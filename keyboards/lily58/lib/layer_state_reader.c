b92377832926bbb6e065394d5b80ea639101deb7
asgard2k <taikamng@yahoo.com>
2020-01-16 01:18:35 -0800

    Commit my changes for lily58

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -9,8 +9,56 @@
 #define L_ADJUST 8
 #define L_ADJUST_TRI 14
 
+// same as in tng's keymap.c
+enum preonic_layers {
+  _QWERTY,
+  _COLEMAK,
+  _DVORAK,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _FUNCTIONS,
+  _MOUSE
+};
+
 char layer_state_str[24];
 
+const char *read_layer_state(void) {
+  switch (biton32(layer_state)) 
+  {
+  case _QWERTY:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
+    break;
+  case _COLEMAK:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: COLEMAK");
+    break;
+  case _DVORAK:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: DVORAK");
+    break;	
+  case _LOWER:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: LOWER");
+    break;
+  case _RAISE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RAISE");
+    break;
+  case _ADJUST:
+  //case L_ADJUST_TRI:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
+    break;
+  case _FUNCTIONS:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: FUNCTIONS");
+    break;	
+case _MOUSE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: MOUSE");
+    break;	
+  default:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+  }
+
+  return layer_state_str;
+}
+
+/*
 const char *read_layer_state(void) {
   switch (layer_state)
   {
@@ -33,3 +81,4 @@ const char *read_layer_state(void) {
 
   return layer_state_str;
 }
+*/
