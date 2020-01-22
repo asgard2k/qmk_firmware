b9cd8363bff16b1227f20b1912cb810497bfd865
asgard2k <taikamng@yahoo.com>
2020-01-22 14:49:21 -0800

    GUI hold on left side
    - add GUI hold on left side on CTRL-BACKSPACE key.

--- keyboards/lily58/keymaps/tng/keymap.c
+++ keyboards/lily58/keymaps/tng/keymap.c
@@ -75,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-	C_S_T(KC_GRV)     , KC_1        , LT(_DVORAK,KC_2)      , KC_3     , KC_4                  , KC_5                                                   , KC_6               , KC_7           , KC_8                     , KC_9           , KC_0             , LCAG_T(KC_ESC) , \
-	LT(_RAISE,KC_TAB) , KC_Q        , KC_W                  , KC_E     , LT(_MOUSE,KC_R)       , KC_T                                                   , KC_Y               , KC_U           , KC_I                     , KC_O           , KC_P             , KC_DEL         , \
-	KC_LSFT           , KC_A        , KC_S                  , KC_D     , LT(_RAISE,KC_F)       , KC_G                                                   , KC_H               , LT(_RAISE,KC_J), KC_K                     , KC_L           , TD(TD_SCLN_CAPS) , RSFT_T(KC_ENT), \
-	KC_LCTL           , KC_Z        , KC_X                  , KC_C     , KC_V                  , KC_B      , LCTL(KC_BSPC)    , LT(_FUNCTIONS,KC_PGDN)  , KC_N               , KC_M           , TD(TD_COMM_CTRLBACKSPACE), KC_DOT         , KC_SLSH          , RCTL_T(KC_QUOT) , \
-	                                                          KC_LALT  , LT(_FUNCTIONS,KC_CAPS), MO(_LOWER), KC_BSPC          , LT(_MOUSE,KC_SPC)       , LT(_RAISE, KC_SPC) , KC_RGUI, KC_RALT\
+	C_S_T(KC_GRV)     , KC_1        , LT(_DVORAK,KC_2)      , KC_3     , KC_4                  , KC_5                                                        , KC_6               , KC_7           , KC_8                     , KC_9           , KC_0             , LCAG_T(KC_ESC) , \
+	LT(_RAISE,KC_TAB) , KC_Q        , KC_W                  , KC_E     , LT(_MOUSE,KC_R)       , KC_T                                                        , KC_Y               , KC_U           , KC_I                     , KC_O           , KC_P             , KC_DEL         , \
+	KC_LSFT           , KC_A        , KC_S                  , KC_D     , LT(_RAISE,KC_F)       , KC_G                                                        , KC_H               , LT(_RAISE,KC_J), KC_K                     , KC_L           , TD(TD_SCLN_CAPS) , RSFT_T(KC_ENT), \
+	KC_LCTL           , KC_Z        , KC_X                  , KC_C     , KC_V                  , KC_B      , LGUI_T(LCTL(KC_BSPC)) , LT(_FUNCTIONS,KC_PGDN)  , KC_N               , KC_M           , TD(TD_COMM_CTRLBACKSPACE), KC_DOT         , KC_SLSH          , RCTL_T(KC_QUOT) , \
+	                                                          KC_LALT  , LT(_FUNCTIONS,KC_CAPS), MO(_LOWER), KC_BSPC               , LT(_MOUSE,KC_SPC)       , LT(_RAISE, KC_SPC) , KC_RGUI, KC_RALT\
 ),
 
 /* COLEMAK
