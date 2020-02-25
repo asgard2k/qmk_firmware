9cc3d2ddb35d78d51415a9822b6d2dcc826eaa03
asgard2k <taikamng@yahoo.com>
2020-02-24 21:21:36 -0800

    Kyria

--- keyboards/kyria/keymaps/tng/keymap.c
+++ keyboards/kyria/keymaps/tng/keymap.c
@@ -202,7 +202,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
-void keyboard_post_init_user(void) {
+// This handler is not working/getting invoked on the Kyria???
+/* void keyboard_post_init_user(void) {
   // Call the post init code.
   
   rgblight_setrgb(RGB_PURPLE);
@@ -211,7 +212,7 @@ void keyboard_post_init_user(void) {
     
   //default_layer_state = QWERTY;
   set_single_persistent_default_layer(_QWERTY);
-}
+} */
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
