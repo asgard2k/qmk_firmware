4bda60191d7ea73e77b96960cb8228474c9dab7b
asgard2k <taikamng@yahoo.com>
2020-03-21 13:06:28 -0700

    Zeta

--- keyboards/zeta/keymaps/tng/keymap.c
+++ keyboards/zeta/keymaps/tng/keymap.c
@@ -44,10 +44,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  C_S_T(KC_TAB),   KC_Q,    KC_W,    KC_E,           LT(_MOUSE,KC_R),   KC_T,                               KC_Y,              KC_U,                    KC_I,                           KC_O,           KC_P,            LCAG_T(KC_ESC), \
-  LSFT_T(KC_CAPS), KC_A,    KC_S,    KC_D,           LT(_RAISE,KC_F),   KC_G,                               KC_H,              LT(_NUMBERS,KC_J),       KC_K,                           KC_L,           KC_SCLN,         RSFT_T(KC_ENT), \
-  KC_LSFT,         KC_Z,    KC_X,    KC_C,           LT(_NUMBERS,KC_V), KC_B,                               KC_N,              LT(_NUMBERS,KC_M),       RALT_T(KC_COMM),                RGUI_T(KC_DOT), RCTL_T(KC_SLSH), MT(MOD_LCTL|MOD_LSFT|MOD_LALT,KC_QUOT), \
-  KC_LCTL,         KC_LGUI, KC_LALT, MO(_FUNCTIONS), MO(_LOWER),        LT(_FUNCTIONS,KC_BSPC),             LT(_RAISE,KC_SPC), LT(_FUNCTIONS,KC_SPC),   MT(MOD_LCTL|MOD_LALT,KC_DEL),   KC_RALT,        KC_RGUI,         KC_RCTL \
+  C_S_T(KC_TAB),   KC_Q,    KC_W,    KC_E,                    LT(_MOUSE,KC_R),   KC_T,                               KC_Y,              KC_U,                    KC_I,                           KC_O,           KC_P,            LCAG_T(KC_ESC), \
+  LSFT_T(KC_CAPS), KC_A,    KC_S,    LT(_NUMBERS_LHALF,KC_D), LT(_RAISE,KC_F),   KC_G,                               KC_H,              LT(_NUMBERS,KC_J),       LT(_NUMBERS_LHALF,KC_K),        KC_L,           KC_SCLN,         RSFT_T(KC_ENT), \
+  KC_LSFT,         KC_Z,    KC_X,    KC_C,                    LT(_NUMBERS,KC_V), KC_B,                               KC_N,              LT(_NUMBERS,KC_M),       RALT_T(KC_COMM),                RGUI_T(KC_DOT), RCTL_T(KC_SLSH), MT(MOD_LCTL|MOD_LSFT|MOD_LALT,KC_QUOT), \
+  KC_LCTL,         KC_LGUI, KC_LALT, MO(_FUNCTIONS),          MO(_LOWER),        LT(_FUNCTIONS,KC_BSPC),             LT(_RAISE,KC_SPC), LT(_FUNCTIONS,KC_SPC),   MT(MOD_LCTL|MOD_LALT,KC_DEL),   KC_RALT,        KC_RGUI,         KC_RCTL \
 ),
 
 
@@ -108,10 +108,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_NUMBERS_LHALF] =  LAYOUT_ortho_4x12( \
-  C_S_T(KC_GRV), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   _______, _______, KC_RSFT, _______, _______, _______, \
-  _______,       KC_4,    KC_5,    KC_6,    _______, _______,                _______, _______, _______, _______, _______, _______, \
-  _______,       KC_7,    KC_8,    KC_9,    KC_0,    _______,                _______, _______, _______, _______, _______, _______, \
-  _______,       _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______ \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+  _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______ \
 )
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
