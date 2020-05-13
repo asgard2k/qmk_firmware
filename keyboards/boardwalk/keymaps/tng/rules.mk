58df8625f55563af2e8db6e551a53a670fa0b62b
asgard2k <taikamng@yahoo.com>
2020-05-12 23:36:03 -0700

    Boardwalk

--- keyboards/boardwalk/keymaps/tng/rules.mk
+++ keyboards/boardwalk/keymaps/tng/rules.mk
@@ -1,4 +1,4 @@
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
