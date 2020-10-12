21c078cfb2780b6afe281044d6303c5b05135412
asgard2k <taikamng@yahoo.com>
2020-10-12 00:40:39 -0700

    ropro

--- keyboards/kingly_keys/ropro/keymaps/tng/keymap.c
+++ keyboards/kingly_keys/ropro/keymaps/tng/keymap.c
@@ -18,24 +18,18 @@
 
 enum layer_names {
   _QWERTY,
-  _COLEMAK,
-  _DVORAK,
   _LOWER,
   _RAISE,
   _ADJUST,
   _FUNCTIONS,
-  _MOUSE,
 };
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
   LOWER,
   RAISE,
   ADJUST,
   FUNCTIONS,
-  MOUSE
 };
 
 #define LOWER MO(_LOWER)
@@ -58,100 +52,98 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------------------------------'
    */
  [_QWERTY] = LAYOUT( \
-              C_S_T(KC_F1),                 LT(_DVORAK,KC_F2), MT(MOD_LCTL|MOD_LSFT|MOD_LALT,KC_F3), KC_F4,                   KC_F5,                  KC_F6,               KC_F7,              KC_F8,            KC_F9,          KC_F10,    KC_F11,          LCAG_T(KC_F12), \
+              KC_F1,                        KC_F2,                              KC_F3,                                 KC_F4,                   KC_F5,                  KC_F6,                  KC_F7,              KC_F8,            KC_F9,           KC_F10,         KC_F11,          LCAG_T(KC_F12), \
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/			  
-              MT(MOD_LCTL|MOD_LALT,KC_GRV), KC_1,              KC_2,                                 KC_3,                    KC_4,                   KC_5,                KC_6,               KC_7,             KC_8,           KC_9,      KC_0,            KC_ESC, \
-    XXXXXXX,  KC_TAB,                       KC_Q,              KC_W,                                 KC_E,                    LT(_MOUSE,KC_R),        KC_T,                KC_Y,               KC_U,             KC_I,           KC_O,      KC_P,            KC_DEL, \
-    KC_HOME,  KC_LSFT,                      KC_A,              KC_S,                                 KC_D,                    LT(_RAISE,KC_F),        KC_G,                KC_H,               LT(_RAISE,KC_J),  KC_K,           KC_L,      KC_SCLN,         RSFT_T(KC_ENT), \
-    KC_PGUP,  LSFT_T(KC_BSPC),              KC_Z,              KC_X,                                 KC_C,                    KC_V,                   KC_B,                KC_N,               KC_M,             KC_COMM,        KC_DOT,    RALT_T(KC_SLSH), RCTL_T(KC_QUOT), \
-    KC_PGDN,  KC_LCTRL,                     KC_LGUI,           KC_LALT,                              LSFT(KC_F10),            LT(_FUNCTIONS,KC_CAPS), LOWER,               LT(_RAISE, KC_SPC), KC_RGUI,          KC_LEFT,        KC_UP,     KC_DOWN,         KC_RIGHT \
+              C_S_T(KC_GRV) ,               MT(MOD_LCTL|MOD_LSFT|MOD_LALT,KC_1), KC_2,                                 KC_3,                    KC_4,                   KC_5,                   KC_6,               KC_7,             KC_8,            KC_9,           KC_0,            KC_ESC, \
+    KC_BSPC,  MT(MOD_LCTL|MOD_LALT,KC_TAB), KC_Q,                                KC_W,                                 KC_E,                    KC_R,                   KC_T,                   KC_Y,               KC_U,             KC_I,            KC_O,           KC_P,            KC_DEL, \
+    KC_ESC,   KC_LSFT,                      KC_A,                                KC_S,                                 KC_D,                    LT(_RAISE,KC_F),        KC_G,                   KC_H,               KC_J,             KC_K,            KC_L,           KC_SCLN,         RSFT_T(KC_ENT), \
+    KC_PGUP,  KC_LSFT,                      KC_Z,                                KC_X,                                 KC_C,                    KC_V,                   KC_B,                   KC_N,               KC_M,             RALT_T(KC_COMM), RGUI_T(KC_DOT), RCTL_T(KC_SLSH), KC_QUOT, \
+    KC_PGDN,  KC_LCTRL,                     KC_LGUI,                             KC_LALT,                              LSFT(KC_F10),            MO(_LOWER),             LT(_FUNCTIONS,KC_BSPC), LT(_RAISE, KC_SPC), MO(_FUNCTIONS),   KC_LEFT,         KC_DOWN,        KC_RIGHT,        KC_UP \
 ),
-
-/* COLEMAK
- */
- [_COLEMAK] = LAYOUT( \
-             _______, _______, _______ , _______ , _______ , _______    , _______ , _______, _______ , _______ , _______ , _______, \
-             _______, _______, _______ , _______ , _______ , _______    , _______ , _______, _______ , _______ , _______ , _______, \
-    XXXXXXX, _______, _______, _______ , _______ , _______ , _______    , _______ , _______, _______ , _______ , _______ , _______, \
-    _______, _______, _______, _______ , _______ , _______ , _______    , _______ , _______, _______ , _______ , _______ , _______, \
-    _______, _______, _______, _______ , _______ , _______ , _______    , _______ , _______, _______ , _______ , _______ , _______, \
-    _______, _______, _______, _______ , _______ , _______ , _______    , _______ , _______, _______ , _______ , _______ , _______ \
- ),
-
-/* DVORAK
- */
- [_DVORAK] = LAYOUT( \
-             _______, _______, _______ , _______, _______, _______,                   _______ , _______, _______, _______, _______, _______, \
-             _______, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    XXXXXXX, _______, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    _______, _______, RGB_TOG, RGB_MOD , RGB_HUI, RGB_SAI, RGB_VAI,                   RGB_TOG, RGB_MOD , RGB_HUI, RGB_SAI, RGB_VAI, _______, \
-    _______, _______, RGB_M_P, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD,                   RGB_M_P, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, \
-    _______, _______, _______, _______ , _______, _______, _______,                   _______, _______ , _______, _______, _______, _______ \
- ),
 
 /* LOWER
  */
  [_LOWER] = LAYOUT( \
-             KC_ESC, _______    , _______, _______,       _______,         _______,                    _______, _______, _______, _______, _______, _______, \
-             KC_ESC , _______    , _______, _______,       _______,        _______,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-    XXXXXXX, _______, LALT(KC_F4), XXXXXXX, LALT(KC_LEFT), LALT(KC_RIGHT), LCTL(LSFT(KC_ESC)),         KC_EXLM, KC_AT  , KC_HASH, KC_LCBR, KC_RCBR, _______, \
-    _______, _______, XXXXXXX    , XXXXXXX, LCTL(KC_PGUP), LCTL(KC_PGDN) , XXXXXXX,                    KC_DLR , KC_LT  , KC_GT  , KC_LBRC, KC_RBRC, _______, \
-    _______, KC_DEL,  XXXXXXX    , XXXXXXX, XXXXXXX,       XXXXXXX,        XXXXXXX,                    _______, KC_PERC, KC_MINS, KC_EQL , KC_BSLS, _______, \
-    _______, _______, _______    , _______, _______,       _______,        _______ ,                   KC_SPC,  _______, _______, _______, _______, _______ \
+             _______, _______    , _______, _______,       _______,        _______,                    _______, _______, _______, _______, _______, _______, \
+             _______, _______    , _______, _______,       _______,        _______,                    _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+    XXXXXXX, _______, LALT(KC_F4), _______, LALT(KC_LEFT), LALT(KC_RIGHT), LCTL(LSFT(KC_ESC)),         _______, _______, _______, KC_LCBR, KC_RCBR, _______, \
+    _______, KC_CAPS, _______    , _______, LCTL(KC_PGUP), LCTL(KC_PGDN) , _______,                    _______, KC_LT  , KC_GT  , KC_LBRC, KC_RBRC, _______, \
+    _______, _______, _______    , _______, _______,       _______,        _______,                    KC_LPRN, KC_RPRN, KC_MINS, KC_EQL , KC_BSLS, _______, \
+    _______, _______, _______    , _______, _______,       _______,        _______,                    KC_SPC,  _______, _______, _______, _______, _______ \
  ),
 
 /* RAISE
  */
  [_RAISE] = LAYOUT( \
-             _______, _______, _______, _______,       _______, _______,        _______,     _______,      _______, _______, _______, _______, \
-             _______, _______, _______, _______,       _______, _______,        _______,     _______,      _______, _______, _______, _______, \
-    XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_CAPS      , XXXXXXX, XXXXXXX,        XXXXXXX,     KC_PGUP,      KC_UP,   KC_PGDN, XXXXXXX, _______, \
-    _______, _______, XXXXXXX, XXXXXXX, LCTL(KC_BSPC), XXXXXXX, XXXXXXX,        KC_HOME,     KC_LEFT,      KC_DOWN, KC_RGHT, KC_END , _______, \
-    _______, KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX      , XXXXXXX, XXXXXXX,        MO(_ADJUST), LSFT(KC_F10), KC_UNDS, KC_PLUS, KC_PIPE, _______, \
-    _______, _______, _______, _______, _______,       _______, _______,        _______,     _______,      _______, _______, _______, _______ \
+             _______, _______,       _______,      _______, _______, _______,        _______,       _______,       _______, _______, _______, _______, \
+             KC_GRV,  KC_1,          _______,      _______, _______, _______,        LCTL(KC_PGUP), LCTL(KC_PGDN), _______, _______, _______, _______, \
+    XXXXXXX, KC_TAB,  _______,       _______,      _______, _______, _______,        _______,       KC_PGUP,       KC_UP,   KC_PGDN, _______, _______, \
+    _______, _______, LCTL(KC_BSPC), LCTL(KC_DEL), KC_DEL,  _______, _______,        KC_HOME,       KC_LEFT,       KC_DOWN, KC_RGHT, KC_END , _______, \
+    _______, _______, _______,       _______,      _______, _______, _______,        MO(_ADJUST),   LSFT(KC_F10),  KC_UNDS, KC_PLUS, KC_PIPE, _______, \
+    _______, _______, _______,       _______,      _______, _______, KC_BSPC,        KC_SPC,        _______,       _______, _______, _______, _______ \
  ),
 
 /* ADJUST
  */
  [_ADJUST] = LAYOUT( \
              _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, \
-             _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    XXXXXXX, _______, RESET  , EEP_RST, DEBUG  , XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, QWERTY , XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+             _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, \
+    XXXXXXX, _______, RESET  , _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______ \
  ),
   
 /* FUNCTIONS
  */
  [_FUNCTIONS] = LAYOUT( \
-             KC_ESC,  _______, _______, _______, _______,    _______,          _______, _______,      _______, _______, _______, _______, \
-             KC_ESC , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,          XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,          XXXXXXX, XXXXXXX,      KC_INS , XXXXXXX, KC_PSCR, LCA(KC_DEL), \
-    _______, _______, XXXXXXX, KC_SLCK, XXXXXXX, XXXXXXX,    XXXXXXX,          XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, KC_PAUS, _______    , \
-    _______, KC_BSPC, XXXXXXX, XXXXXXX, XXXXXXX, LCTL(KC_V), XXXXXXX,          KC_NLCK, LSFT(KC_F10), XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+             _______, _______, _______, _______, _______,    _______,          _______, _______,      _______, _______, _______, _______, \
+             _______, _______, _______, _______, _______,    _______,          _______, _______,      _______, _______, _______, _______, \
+    KC_ENT,  _______, _______, _______, _______, _______,    _______,          _______, _______,      KC_INS , _______, KC_PSCR, LCA(KC_DEL), \
+    _______, KC_CAPS, _______, _______, _______, _______,    _______,          _______, _______,      _______, _______, _______, _______    , \
+    _______, _______, _______, _______, _______, LCTL(KC_V), _______,          _______, _______,      _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______,    _______,          _______, _______,      _______, _______, _______, _______ \
- ),
-  
-/* MOUSE
- */
- [_MOUSE] = LAYOUT( \
-             _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, \
-             _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    XXXXXXX, _______, KC_ACL0, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,          KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_ACL0, _______, \
-    _______, _______, KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,          KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1, _______, \
-    _______, _______, KC_ACL2, KC_WH_L, XXXXXXX, KC_WH_R, XXXXXXX,          XXXXXXX, KC_WH_L, XXXXXXX, KC_WH_R, KC_ACL2, _______, \
-    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______ \
  ),
 
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_WH_L);
-        } else {
-            tap_code(KC_WH_R);
+    if (index == 0) 
+	{
+	  if(IS_LAYER_ON(_RAISE)) 
+	  { 
+        if (clockwise) 
+		{
+            tap_code(KC_PGDN);
+        } 
+		else 
+		{
+            tap_code(KC_PGUP);
         }
+	  } 
+	  else if(IS_LAYER_ON(_FUNCTIONS)) 
+	  { 
+        if (clockwise) 
+		{
+			// Move wheel left
+            tap_code(KC_DOWN);
+        } 
+		else 
+		{
+            tap_code(KC_UP);
+        }
+	  } 
+	  else 
+	  { 
+		if (clockwise)
+		{
+		  tap_code(KC_WH_D);
+		} 
+		else
+		{
+		  // Move wheel up
+		  tap_code(KC_WH_U);
+		}
+	  }		
     }
 }
--- keyboards/kingly_keys/ropro/rules.mk
+++ keyboards/kingly_keys/ropro/rules.mk
@@ -14,10 +14,10 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
-CONSOLE_ENABLE = no        # Console for debug(+400)
+BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
@@ -27,5 +27,5 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 ENCODER_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-LAYOUTS_HAS_RGB = yes
+RGBLIGHT_ENABLE = no
+LAYOUTS_HAS_RGB = no
